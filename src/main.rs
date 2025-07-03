4c3d1d819e47e0b5195f69d0199841a2c4f39f5b
Doug Cooper <douglas.cooper@ngc.com>
2025-07-03 14:07:38 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    use ctor::dtor;
+    use rstest::{fixture, rstest};
+    use lazy_static::lazy_static;
+
+
+    struct Foo{
+        bar: i32
+    }
+
+    lazy_static! {
+        static ref FOO: Arc<Mutex<Foo>> = Arc::new(Mutex::new(Foo { bar: 42 })); // Foo = Foo{bar: 42};
+    }
+
+    #[fixture]
+    #[once]
+    fn foo() -> &'static Arc<Mutex<Foo>> {
+        &FOO
+    }
+
+    #[rstest]
+    fn it_works(foo: &Arc<Mutex<Foo>>) {
+        assert_eq!(foo.lock().unwrap().bar, 42);
+        foo.lock().unwrap().bar = 0;
+    }
+
+    #[dtor]
+    fn cleanup() {
+        assert_eq!(FOO.lock().unwrap().bar, 0);
+    }
+}
